f9162aceaacb65bdd1bdb10328c6cd9fbcdfc5cc
EC2 Default User <ec2-user@ip-172-31-11-167.us-east-2.compute.internal>
2020-03-29 14:44:46 +0000

    initial commit

---
+++ src/build_full_distance_matrix.py
@@ -0,0 +1,91 @@
+from keras.preprocessing import image as image_utils
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.applications import vgg16, inception_v3, resnet50, mobilenet
+from keras import backend as K
+import keras
+import numpy as np
+
+#Load the VGG model    
+model = vgg16.VGG16(weights='imagenet')
+
+
+#image_path = ""
+#original_iamge = cv2.imread(image_path)
+
+
+layer_names = [layer.name for layer in model.layers]
+
+test_image_path = 'data/flickr30k_images/flickr30k_images/flickr30k_images/996089206.jpg'
+
+def display_image(image_path):
+    import cv2
+    
+    image = cv2.imread(image_path)
+    img = cv2.imread(test_image_path)
+    img2 = img[:,:,::-1]
+    plt.imshow(img2)
+
+#----- flow for single image
+def load_image(image_path):
+    image = image_utils.load_img(image_path, target_size=(224, 224))
+    image = image_utils.img_to_array(image)
+
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+    return image
+
+def get_intermediate_model(layer_name, model):
+    intermediate_layer_model = Model(inputs=model.input,
+                             outputs=model.get_layer(layer_name).output)
+    return intermediate_layer_model
+    
+def predict_image(model,image):
+    preds = model.predict(image)
+    #preds = decode_predictions(preds)
+    return preds
+# trunc_model = get_intermediate_model(layer_name, full_model)
+# img = load_image(image_path)
+# out = predict_image(trunc_model,img)
+#-----
+
+def get_image_flow(data_path, batch_size = 32, target_size = (224,224), **kwargs):
+    datagen = ImageDataGenerator()
+    flow = datagen.flow_from_directory(data_path,
+                                       target_size=target_size,
+                                       batch_size=batch_size,
+                                       **kwargs)
+    return flow
+
+def predict_flow(model,flow,steps = 1, **kwargs):
+    return model.predict_generator(generator=flow,steps = steps,**kwargs)
+
+# trunc_model = get_intermediate_model(layer_name, full_model)
+# flow = get_image_flow('data/flickr30k_images/flickr30k_images/')
+# out = predict_flow(trunc_model, flow)
+
+def get_distance_matrix(model_outputs):
+    from sklearn.metrics.pairwise import euclidean_distances
+    return euclidean_distances(model_outputs,model_outputs)
+
+def draw_heatmap(distance_matrix, ax = None):
+    import seaborn as sns
+    ax = sns.heatmap(distance_matrix,cmap="inferno", ax = ax)
+    return ax
+
+# distance_matrix = get_distance_matrix(out)
+# draw_heatmap(distance_matrix)
+
+#----
+
+
+for layer_name in layer_names[20:]:
+    trunc_model = get_intermediate_model(layer_name, model)
+    flow = get_image_flow('data/flickr30k_images/flickr30k_images/')
+    out = predict_flow(trunc_model, flow, steps = 1000)
+    distance_matrix = get_distance_matrix(out)
+    np.savetxt('./outputs/'+layer_name+'_output.txt',out)
+
+    
